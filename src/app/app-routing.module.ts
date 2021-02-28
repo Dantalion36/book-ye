b65cf68d6db7444b1af35192c0537d980dbaa4fd
Dantalion36 <logayd@gmail.com>
2021-02-28 12:38:28 +0200

    basic architecture + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+import { DistributionComponent } from './pages/distribution/distribution.component';
+import { ProductComponent } from './pages/product/product.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'catalogue', component: CatalogueComponent },
+  { path: 'distribution', component: DistributionComponent },
+  { path: 'product', component: ProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+import { DistributionComponent } from './pages/distribution/distribution.component';
+import { ProductComponent } from './pages/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactsComponent,
+    CatalogueComponent,
+    DistributionComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
